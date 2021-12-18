ebd593ef5efdb2d6c872c51d4a6e842b79a88c9a
alirrah81 <alir.rah81@gmail.com>
2021-12-18 12:14:15 +0330

    binary expression tree and show it

--- project.py
+++ project.py
@@ -9,6 +9,13 @@ class nodeStack:
         self.next = next
 
 
+class nodeTree:
+    def __init__(self, data, right, left):
+        self.data = data
+        self.right = right
+        self.left = left
+
+
 class Stack:
 
     def __init__(self):
@@ -202,21 +209,96 @@ def evalPostfix(pharse):
         return 'error'
 
 
+def makeTree(l):
+    stack = Stack()
+    tmp = l.split(' ')
+    for i in range(len(tmp)):
+        tmp[i] = nodeTree(tmp[i], None, None)
+    for item in tmp:
+        if item.data == '*' or item.data == '/' or item.data == '^' or item.data == '+' or item.data == '-':
+            first = stack.pop()
+            second = stack.pop()
+            item.left = second
+            item.right = first
+            stack.push(item)
+        else:
+            stack.push(item)
+    return stack.top()
+
+
+def print_tree(root, data="data", left="left", right="right"):
+    def display(root, data=data, left=left, right=right):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if getattr(root, right) is None and getattr(root, left) is None:
+            line = '%s' % getattr(root, data)
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if getattr(root, right) is None:
+            lines, n, p, x = display(getattr(root, left))
+            s = '%s' % getattr(root, data)
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if getattr(root, left) is None:
+            lines, n, p, x = display(getattr(root, right))
+            s = '%s' % getattr(root, data)
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = display(getattr(root, left))
+        right, m, q, y = display(getattr(root, right))
+        s = '%s' % getattr(root, data)
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * \
+            '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + \
+            (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + \
+            [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+    lines, *_ = display(root, data, left, right)
+    for line in lines:
+        print(line)
+
+
 while True:
     sentence = input('Enter the phrase (exit to end the program) : ')
     if sentence == 'exit':
         break
     sentence = infixToPostfix(sentence)
+    tmp = sentence
+    print()
     if sentence == 'error':
-        print(sentence)
+        print(sentence, end = '\n\n')
     else:
         sentence = evalPostfix(sentence)
         if sentence == 'error':
-            print(sentence)
+            print(sentence, end = '\n\n')
         else:
+            tree = makeTree(tmp)
+            print_tree(tree)
             print('\nHistory : ')
             for i in sentence:
                 print(i)
-            print('\nAnswer is : ' + sentence[-1])
+            print('\nAnswer is : ' + sentence[-1], end = '\n\n')
     os.system('pause')
     os.system('cls')
